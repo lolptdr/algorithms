2204fa39184d5f679fa42722332c04466f51405f
Joseph Tingsanchali <charnwoot@gmail.com>
2014-08-01 01:43:35 -0500

    more testing for scrubbing data in complex predictor

--- lib/complex_predictor.rb
+++ lib/complex_predictor.rb
@@ -16,20 +16,24 @@ class ComplexPredictor < Predictor
         words: 0,
         books: 0
       }
+      @counts[category] = Hash.new(0)
       books.each do |filename, tokens|
         @data[category][:words] += tokens.count
         @data[category][:books] += 1
-        # @opto[filename] = tokens.count
-        test = books[0][1]
-        test1 = test.delete_if { |x| x =~ /\d/ }
-        test2 = test1.uniq
-        test3 = test2.each { |x| x.gsub!(/_/,'') }
         
+        # test = books[index][index+1]
+        # test1 = test.delete_if { |x| x =~ /\d/ }
+        # test2 = test1.uniq
+        # test3 = test2.each { |x| x.gsub!(/_/,'') }
+        # test4 = test3.delete_if { |x| x.length < 3 }
+        # @counts[category][filename] = test4.length
+
         # test3.each { |x| @counts[x] += 1 }
 binding.pry
       end
-
+# binding.pry
     end
+# binding.pry
   end
 
   # Public: Predicts category.
